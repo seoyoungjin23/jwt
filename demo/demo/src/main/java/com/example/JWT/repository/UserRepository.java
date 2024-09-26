bd9760766f875b4b88a155f87be9f2e566eb7bbb
seoyoungjin23 <ascd050@gmail.com>
2024-09-26 19:11:54 +0900

    entity repository 생성

---
+++ demo/demo/src/main/java/com/example/JWT/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.JWT.repository;
+
+import com.example.JWT.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
