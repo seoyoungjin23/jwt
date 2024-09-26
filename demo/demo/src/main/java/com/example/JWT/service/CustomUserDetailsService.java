de0bdb712417ffbeb17172b6c9399ed1498cd8a6
seoyoungjin23 <ascd050@gmail.com>
2024-09-26 19:32:16 +0900

    DB 로그인 검증 로직

---
+++ demo/demo/src/main/java/com/example/JWT/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.JWT.service;
+
+import com.example.JWT.entity.UserEntity;
+import com.example.JWT.repository.UserRepository;
+import com.example.JWT.util.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
